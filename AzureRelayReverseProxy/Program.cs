2660bf585d753762cb82a38c049ead8f2903fa84
John Watson <johnwatson484@hotmail.com>
2021-05-24 19:43:54 +0100

    Initial

---
+++ AzureRelayReverseProxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureRelayReverseProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
